2e76a387c63c7204b3f87c9b53faf46335a96df0
Ryan James Spencer <spencer.ryanjames@gmail.com>
2019-02-25 15:30:34 +1100

    Remove stub test

--- g_lib/src/lib.rs
+++ g_lib/src/lib.rs
@@ -258,11 +258,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
     fn target_cache_builds() {
         match TargetCache::new().build() {
             Ok(x) => println!("{:#?}", x),
